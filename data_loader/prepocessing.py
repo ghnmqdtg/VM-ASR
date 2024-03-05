3ea34f25d86eef5e4d8d1a1f3f0d4c6b4ac66317
ghnmqdtg <ghnmqdtg@gmail.com>
2024-03-05 20:06:37 +0800

    Feat: Add preprocessing functions to simulate low-resolution samples

---
+++ data_loader/prepocessing.py
@@ -0,0 +1,290 @@
+import time
+import torch
+import torchaudio
+import random
+import matplotlib.pyplot as plt
+from scipy.signal import cheby1
+from scipy.signal import sosfiltfilt
+
+try:
+    from utils import ensure_dir
+except:
+    import os
+    import sys
+    # Used for debugging data_loader
+    # Add the project root directory to the Python path
+    project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    sys.path.append(project_root)
+    from utils import ensure_dir
+
+
+def get_mag_phase(waveform):
+    """
+    Apply short time Fourier transform to the waveform and return the magnitude and phase
+
+    Args:
+        waveform (torch.Tensor): The waveform
+
+    Returns:
+        torch.Tensor: The magnitude
+        torch.Tensor: The phase
+    """
+    # TODO: Set the parameters from the config file
+    # Apply short time Fourier transform to the waveform
+    spec = torch.stft(waveform, n_fft=2048, hop_length=512, win_length=2048,
+                      window=torch.hann_window(2048), return_complex=True)
+    # View the spec as real tensor
+    spec = torch.view_as_real(spec)
+    real = spec[..., 0]
+    imag = spec[..., 1]
+    # Get the magnitude and phase
+    mag = torch.sqrt(real ** 2 + imag ** 2)
+    phase = torch.atan2(imag, real)
+    # Return the magnitude and phase
+    return mag, phase
+
+
+def plot_all(waveform, sample_rate, filename):
+    """
+    Plot the waveform, magnitude and phase in a row at the same figure and save it to the output folder
+
+    Args:
+        waveform (torch.Tensor): The waveform
+        sample_rate (int): The sample rate
+        filename (str): The filename of the output figure
+
+    Returns:
+        None
+    """
+    # Get the magnitude and phase
+    mag, phase = get_mag_phase(waveform)
+    # Plot the waveform, magnitude and phase
+    plt.figure(figsize=(12, 4))
+    # Add title to the figure
+    plt.suptitle(f"Waveform, Magnitude and Phase at {sample_rate} Hz")
+    plt.subplot(1, 3, 1)
+    plt.plot(waveform.t().numpy())
+    plt.title("Waveform")
+    plt.subplot(1, 3, 2)
+    plt.imshow(mag.log2().numpy().squeeze(0), aspect='auto', origin='lower')
+    plt.title("Magnitude")
+    plt.subplot(1, 3, 3)
+    plt.imshow(phase.numpy().squeeze(0), aspect='auto', origin='lower')
+    plt.title("Phase")
+    plt.savefig(filename)
+    plt.close()
+
+
+def low_pass_filter(waveform, sr_org, sr_new):
+    """
+    Apply low pass filter to the waveform to remove the high frequency components.
+    This can avoid aliasing when downsampling the waveform.
+
+    Args:
+        waveform (torch.Tensor): The input waveform
+        sr_org (int): The original sample rate
+        sr_new (int): The new sample rate
+
+    Returns:
+        torch.Tensor: The filtered waveform
+    """
+    # Define the cutoff frequency and the ratio to the Nyquist frequency
+    nyquist = sr_org / 2
+    highcut = sr_new // 2
+    hi = highcut / nyquist
+    # TODO: Set filter funtion as a parameter
+    # Setup the low pass filter: chebyshev type 1
+    sos = cheby1(8, 0.1, hi, btype='lowpass', output='sos')
+    # Apply the filter
+    waveform = sosfiltfilt(sos, waveform)
+    # Convert the waveform to tensor
+    waveform_tensor = torch.tensor(waveform.copy(), dtype=torch.float32)
+    # Return the waveform
+    return waveform_tensor
+
+
+def resample_audio(waveform, sr_org, sr_new):
+    """
+    Downsample the waveform to the new sample rate
+
+    Args:
+        waveform (torch.Tensor): The input waveform
+        sr_org (int): The original sample rate
+        sr_new (int): The new sample rate
+
+    Returns:
+        torch.Tensor: The downsampled waveform
+    """
+    waveform_downsampled = torchaudio.transforms.Resample(
+        sr_org, sr_new)(waveform)
+    return waveform_downsampled
+
+
+def low_sr_simulation(waveform, sr_org, sr_new):
+    """
+    Simulate the low sample rate
+
+    Args:
+        waveform (torch.Tensor): The input waveform
+        sr_org (int): The original sample rate
+        sr_new (int): The new sample rate
+
+    Returns:
+        torch.Tensor: The low sample rate waveform
+    """
+    # Apply low pass filter to the waveform
+    waveform_lr = low_pass_filter(waveform, sr_org, sr_new)
+    # Downsample the waveform
+    waveform_lr = resample_audio(waveform_lr, sr_org, sr_new)
+    # Return the low sample rate waveform
+    return waveform_lr
+
+
+def cut2chunks(waveform: torch.Tensor, chunk_size: int, overlap: int, return_padding_length: bool = False) -> list[torch.Tensor] | tuple[list[torch.Tensor], int]:
+    """
+    Cut the waveform into chunks with the specified size and overlap.
+
+    Args:
+        waveform (torch.Tensor): The input waveform
+        chunk_size (int): The size of each chunk
+        overlap (int): The overlap between chunks
+        return_padding_length (bool, optional): Whether to return the padding length. Defaults to False.
+
+    Returns:
+        List[torch.Tensor]: A list of chunks
+        padding_length (int, optional): The length of the padding. Defaults to None.
+    """
+    # Initialize the list of chunks
+    chunks = []
+    # Get the length of the waveform
+    length = waveform.size(-1)
+    # Step size
+    step_size = chunk_size - overlap
+
+    start = 0
+    padding_length = 0
+    # Cut the waveform into chunks, finish when the last chunk is smaller than chunk_size
+    while start + chunk_size <= length:
+        chunks.append(waveform[..., start:start + chunk_size])
+        start += step_size
+    
+    # Pad the last chunk with zeros
+    if start < length:
+        last_chunk = waveform[..., start:]
+        # If padding is required, pad the last chunk to the chunk_size
+        padding_length = chunk_size - last_chunk.size(-1)
+        last_chunk = torch.nn.functional.pad(last_chunk, (0, padding_length), 'constant', 0)
+        chunks.append(last_chunk)
+
+    print(f"Length: {length}, Chunk number: {len(chunks)}, Padding length: {padding_length}")
+
+    if return_padding_length:
+        return chunks, padding_length
+    else:
+        return chunks
+
+
+def low_sr_simulation_pipeline(waveform: torch.Tensor, sr_org: int, sr_new: int | list[int], chunk_size: int, overlap: int) -> list[torch.Tensor]:
+    """
+    Apply the low sample rate simulation pipeline to the waveform
+
+    Args:
+        waveform (torch.Tensor): The input waveform
+        sr_org (int): The original sample rate
+        sr_new (int | list[int]): The new sample rate
+        chunk_size (int): The size of each chunk
+        overlap (int): The overlap between chunks
+
+    Returns:
+        List[torch.Tensor]: A list of chunks
+    """
+    # Check if sr_new is a list
+    if isinstance(sr_new, list):
+        # Randomly select a new sample rate
+        sr_new = random.choice(sr_new)
+    # Apply the low sample rate simulation
+    waveform_lr = low_sr_simulation(waveform, sr_org, sr_new)
+    # Cut the waveform into chunks
+    chunks = cut2chunks(waveform_lr, chunk_size, overlap)
+    # Return the list of chunks
+    return chunks
+
+def concatenate_chunks(chunks: list[torch.Tensor], chunk_size: int, overlap: int, padding_length) -> torch.Tensor:
+    """
+    Concatenate the chunks into a single waveform by averaging the overlap regions and removing the padding of the last chunk.
+
+    Args:
+        chunks (List[torch.Tensor]): A list of chunks
+        chunk_size (int): The size of each chunk
+        overlap (int): The overlap between chunks
+        padding_length (int): The length of the padding for the last chunk
+
+    Returns:
+        torch.Tensor: The concatenated waveform
+    """
+    # Return the concatenated waveform if it's empty
+    if not chunks:
+        return torch.tensor([])
+    # Adjust total_length calculation
+    if len(chunks) > 1:
+        total_length = chunk_size + (len(chunks) - 1) * (chunk_size - overlap) - padding_length
+    else:
+        total_length = chunk_size - padding_length
+
+    # Initialize the concatenated waveform with the first chunk
+    concatenated = chunks[0].clone()
+    # Iterate through the rest of the chunks and concatenate them
+    for i in range(1, len(chunks)):
+        curr_chunk = chunks[i]
+        # Handle overlap by averaging
+        if overlap > 0:
+            averaged_overlap = (concatenated[:, -overlap:] + curr_chunk[:, :overlap]) / 2 
+            concatenated = torch.cat([concatenated[:, :-overlap], averaged_overlap, curr_chunk[:, overlap:]], dim=-1)
+        else:
+            concatenated = torch.cat([concatenated, curr_chunk], dim=-1)
+
+    print(f"Total length: {total_length}, Concatenated length: {concatenated.size(-1)}, Padding length: {padding_length}")
+
+    # Remove the padding from the last chunk if any
+    concatenated = concatenated[..., :total_length]
+
+    # Return the concatenated waveform
+    return concatenated
+
+
+if __name__ == '__main__':
+    # Test the toy_load function
+    filepath = "./data/VCTK-Corpus-0.92/wav48_silence_trimmed_wav/p225/p225_001.wav"
+    # Make sure the output folder exists
+    ensure_dir("./output/dev/data_preprocessing")
+    # Set the parameters
+    # List of target sample rates to choose from
+    target_sample_rates = [8000, 16000, 24000]
+    chunk_size = 2048  # Size of each audio chunk
+    overlap = 512  # Overlap size between chunks
+    # Apply the audio preprocessing pipeline
+    sr_new = random.choice(target_sample_rates)
+    print(f"Randomly selected new sample rate: {sr_new} Hz")
+    # Apply the audio preprocessing pipeline
+    waveform, sr_org = torchaudio.load(filepath)
+    waveform_filtered = low_pass_filter(waveform, sr_org, sr_new)
+    waveform_downsampled = resample_audio(waveform_filtered, sr_org, sr_new)
+    chunks, padding_length = cut2chunks(waveform_downsampled, chunk_size, overlap, return_padding_length=True)
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    # Plot the waveform, magnitude and phase
+    plot_all(waveform_downsampled, sr_new,
+             f"./output/dev/data_preprocessing/lr_simulation_{sr_new}_{timestr}.png")
+    # Save the chunks to the output folder
+    for i, chunk in enumerate(chunks):
+        torchaudio.save(
+            f"./output/dev/data_preprocessing/chunk_{timestr}_{i}_{sr_new}.wav", chunk, sr_new)
+    print(
+        f"Processed {len(chunks)} chunks from {filepath} at {sr_new} Hz sample rate")
+    
+    # Reconstruct the waveform from the chunks
+    waveform_reconstructed = concatenate_chunks(chunks, chunk_size, overlap, padding_length)
+    # Upsample the reconstructed waveform
+    waveform_reconstructed = resample_audio(waveform_reconstructed, sr_new, sr_org)
+    # Save the reconstructed waveform
+    torchaudio.save(
+        f"./output/dev/data_preprocessing/reconstructed_{timestr}_{sr_new}.wav", waveform_reconstructed, sr_org)
