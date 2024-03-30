b01dc7ffd2cac3d219bebbd03652331e2a3e35e4
ghnmqdtg <ghnmqdtg@gmail.com>
2024-03-30 23:56:45 +0800

    Feat: Add `CosineAnnealingWarmupRestarts` as a scheduler

--- train.py
+++ train.py
@@ -10,6 +10,7 @@ import model.model as module_arch
 from parse_config import ConfigParser
 from trainer import trainer_v1, trainer_v2
 from utils import prepare_device
+from utils.scheduler import CosineAnnealingWarmupRestarts
 import wandb
 
 
@@ -55,7 +56,15 @@ def main(config):
     # Build optimizer, learning rate scheduler. Delete every lines containing lr_scheduler for disabling scheduler
     trainable_params = filter(lambda p: p.requires_grad, model.parameters())
     optimizer = config.init_obj("optimizer", torch.optim, trainable_params)
-    lr_scheduler = config.init_obj("lr_scheduler", torch.optim.lr_scheduler, optimizer)
+    if config["lr_scheduler"]["type"] == "CosineAnnealingWarmupRestarts":
+        lr_scheduler = CosineAnnealingWarmupRestarts(
+            optimizer,
+            **config["lr_scheduler"]["args"],
+        )
+    else:
+        lr_scheduler = config.init_obj(
+            "lr_scheduler", torch.optim.lr_scheduler, optimizer
+        )
 
     _TRAINER = dict(
         v1=trainer_v1.Trainer,
