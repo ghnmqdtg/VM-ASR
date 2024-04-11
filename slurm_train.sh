12fb92cc7310b9df25f7078a50ee047f7c3542bb
ghnmqdtg <ghnmqdtg@gmail.com>
2024-04-11 16:21:54 +0800

    Misc: Add training history config files

--- slurm_train.sh
+++ slurm_train.sh
@@ -15,4 +15,9 @@
 # python train.py --config config/config_20240405_v2_chunk_MAE.json
 # python train.py --config config/config_20240405_v2_chunk_MSE.json
 # python train.py --config config/config_20240407_v2_chunk_MSE_fullband.json
-python train.py --config config/config_20240407_v2_chunk_MAE_fullband.json
+# python train.py --config config/config_20240408_v2_chunk_MAE_fullband.json
+# python train.py --config config/config_20240409_v2_chunk_MAE_fullband_fulldata.json
+# python train.py --config config/config_20240410_v2_chunk_MSE_large.json
+# python train.py --config config/config_20240410_v2_chunk_MAE_ouputv1.json
+# python train.py --config config/config_20240410_v2_chunk_MAE_ouputv2.json
+python train.py --config config/config_20240411_cat.json
