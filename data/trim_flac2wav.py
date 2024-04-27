811d2462b17886c7b1bcd3d31c4cf4f41307e058
ghnmqdtg <ghnmqdtg@gmail.com>
2024-04-27 12:45:57 +0800

    Fix: Do not overwrite existing trimmed audio files

--- data/trim_flac2wav.py
+++ data/trim_flac2wav.py
@@ -30,12 +30,12 @@ if __name__ == "__main__":
     print(dataset[0])
 
     # Set the destination path
-    dest_path = os.path.join(
+    trimmed_folder = os.path.join(
         config["data_loader"]["args"]["data_dir"],
         "VCTK-Corpus-0.92/wav48_silence_trimmed_wav",
     )
     # Make sure the output directory exists, if not, create it
-    ensure_dir(dest_path)
+    ensure_dir(trimmed_folder)
 
     # Read the timestamps txt as pandas dataframe
     timestamps = pd.read_csv(config["flac2wav"]["timestamps"], sep=" ", header=None)
@@ -56,21 +56,22 @@ if __name__ == "__main__":
         flac_name = f"{speaker_id}_{utterance_id}"
         # Get the if from the timestamps dataframe
         flac_timestamps = timestamps[timestamps["filename"] == flac_name]
+        # Set the destination file folder
+        dest_folder = os.path.join(trimmed_folder, f"{speaker_id}")
         # Set the destination file path
-        dest_folder = os.path.join(dest_path, f"{speaker_id}")
+        dest_path = os.path.join(dest_folder, f"{flac_name}.wav")
         # Make sure the output directory exists, if not, create it
         ensure_dir(dest_folder)
+        # Check if the destination file already exists
+        if os.path.exists(dest_path):
+            continue
         # If the flac file is not in the timestamps, directly save the waveform
         if flac_timestamps.empty:
-            torchaudio.save(
-                os.path.join(dest_folder, f"{flac_name}.wav"), waveform, sample_rate
-            )
+            torchaudio.save(dest_path, waveform, sample_rate)
             continue
         # Trim the waveform tensor based on the start and end of timestamps
         start = flac_timestamps["start"].values[0]
         end = flac_timestamps["end"].values[0]
         trimmed_waveform = waveform[:, start:end]
         # Save the trimmed waveform
-        torchaudio.save(
-            os.path.join(dest_folder, f"{flac_name}.wav"), trimmed_waveform, sample_rate
-        )
+        torchaudio.save(dest_path, trimmed_waveform, sample_rate)
