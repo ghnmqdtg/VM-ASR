2006c04089ce0870095d97d0e5c43ea793a0287a
ghnmqdtg <ghnmqdtg@gmail.com>
2024-05-01 16:01:11 +0800

    Feat: Remove JSON config from logger

--- logger/logger.py
+++ logger/logger.py
@@ -1,26 +1,40 @@
+import os
+import sys
 import logging
-import logging.config
-from pathlib import Path
-from utils import read_json
+import functools
+from termcolor import colored
 
 
-def setup_logging(
-    save_dir, log_config="logger/logger_config.json", default_level=logging.INFO
-):
-    """
-    Setup logging configuration
-    """
-    log_config = Path(log_config)
-    if log_config.is_file():
-        config = read_json(log_config)
-        # modify logging paths based on run config
-        for _, handler in config["handlers"].items():
-            if "filename" in handler:
-                handler["filename"] = str(save_dir / handler["filename"])
+@functools.lru_cache()
+def create_logger(output_dir, dist_rank=0, name=""):
+    # create logger
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    logger.propagate = False
 
-        logging.config.dictConfig(config)
-    else:
-        print(
-            "Warning: logging configuration file is not found in {}.".format(log_config)
+    # create formatter
+    fmt = "[%(asctime)s %(name)s] (%(filename)s %(lineno)d): %(levelname)s %(message)s"
+    color_fmt = (
+        colored("[%(asctime)s %(name)s]", "green")
+        + colored("(%(filename)s %(lineno)d)", "yellow")
+        + ": %(levelname)s %(message)s"
+    )
+
+    # create console handlers for master process
+    if dist_rank == 0:
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setLevel(logging.DEBUG)
+        console_handler.setFormatter(
+            logging.Formatter(fmt=color_fmt, datefmt="%Y-%m-%d %H:%M:%S")
         )
-        logging.basicConfig(level=default_level)
+        logger.addHandler(console_handler)
+
+    # create file handlers
+    file_handler = logging.FileHandler(
+        os.path.join(output_dir, f"log_rank{dist_rank}.txt"), mode="a"
+    )
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(logging.Formatter(fmt=fmt, datefmt="%Y-%m-%d %H:%M:%S"))
+    logger.addHandler(file_handler)
+
+    return logger
